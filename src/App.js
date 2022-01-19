e7dda19fd04a6e18d03bc32958b30a621d2398c4
iskender-crypto <iskenderazamov@gmail.com>
2022-01-19 12:15:14 +0600

    Branch ачылды

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          nabiev00
-        </p>
+        <p>Iskender</p>
         <a
           className="App-link"
           href="https://reactjs.org"
